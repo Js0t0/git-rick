1770b1f251490356bb749e5a82cfed8d857a2a43
JefriS0t0 <76411328@certus.edu.pe>
2024-03-05 17:05:51 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import CharacterList from "./components/CharacterList";
+import banner from '../src/Rick-and-Morty.png';
 
 function App() {
   return (
     <div >
        <a >
-        <img src="src\assets\Rick-and-Morty.png"
+        <img src={banner}
         style={{ width: "1000px", height: "300px" }}
         className="rounded mx-auto d-block"
         
