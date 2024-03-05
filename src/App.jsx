14b693673a4f035ebfa7088a5656dde7aebd6f8d
JefriS0t0 <76411328@certus.edu.pe>
2024-03-05 16:59:42 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import CharacterList from "./components/CharacterList";
 function App() {
   return (
     <div >
-      <a href='reload'><img
-        src="src\assets\Rick-and-Morty.png"
+       <a >
+        <img src="src\assets\Rick-and-Morty.png"
         style={{ width: "1000px", height: "300px" }}
-        class="rounded mx-auto d-block"
+        className="rounded mx-auto d-block"
         
       ></img></a>
+     
       <CharacterList />
       
     </div>
